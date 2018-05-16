711153730dd38dac8ac5045d27c7121e95bb02aa
Simon Shields <simon@lineageos.org>
2018-05-16 15:38:09 +1000

    midas: temporarily just use the i9300 dtb

--- board/midas/post-image.sh
+++ board/midas/post-image.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${BR2_EXTERNAL_MIDAS_PATH}/util/mkbootimg --kernel ${BINARIES_DIR}/zImage.exynos4412-midas --ramdisk ${BINARIES_DIR}/rootfs.cpio.lzma --cmdline "ignored by bootloader" --base 0x40000000 --pagesize 2048 -o ${BINARIES_DIR}/boot.img
+${BR2_EXTERNAL_MIDAS_PATH}/util/mkbootimg --kernel ${BINARIES_DIR}/zImage.exynos4412-i9300 --ramdisk ${BINARIES_DIR}/rootfs.cpio.lzma --cmdline "console=/dev/tty0" --base 0x40000000 --pagesize 2048 -o ${BINARIES_DIR}/boot.img
