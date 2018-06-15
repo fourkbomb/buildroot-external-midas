5425151dfa569ec7cc1415a83b2c14a6b5083364
Simon Shields <simon@lineageos.org>
2018-06-16 00:30:12 +1000

    midas: build a u-boot fit image

---
+++ board/midas/create_4412_emmc_image.sh
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2010 Samsung Electronics Co., Ltd.
+#              http://www.samsung.com/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+####################################
+if [ $# -lt 4 ]
+then
+    echo "usage: ./create_4412_sd_image.sh <fwbl1> <spl> <u-boot> <output file>"
+    echo "usage: ./create_4412_emmc_image.sh <output file>"
+    exit 0
+fi
+
+block_size=512
+#sizes are multiplied by block_size
+#8kB
+fwbl1_size=16
+#16kB
+bl2_size=32
+#1024kB
+uboot_size=2048
+#16kB
+env_size=32
+
+fwbl1_pos=0
+bl2_pos=$(($fwbl1_pos + $fwbl1_size))
+uboot_pos=$(($bl2_pos + $bl2_size))
+env_pos=$(($uboot_pos + $uboot_size))
+#env_pos=$(($uboot_pos + $uboot_size))
+
+####################################
+echo "Exynos4412 FWBL1 fusing"
+dd if=$1 of=$4 bs=$block_size seek=$fwbl1_pos
+####################################
+echo "Exynos4412 BL2 fusing"
+dd if=$2 of=$4 bs=$block_size seek=$bl2_pos
+####################################
+echo "Exynos4412 bootloader fusing"
+dd if=$3 of=$4 bs=$block_size seek=$uboot_pos
+####################################
+
+sync
+#<Message Display>
+echo "$4 - Exynos4412 U-Boot EMMC boot image is ready."
--- board/midas/post-image.sh
+++ board/midas/post-image.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
-${BR2_EXTERNAL_MIDAS_PATH}/util/mkbootimg --kernel ${BINARIES_DIR}/zImage.exynos4412-i9300 --ramdisk ${BINARIES_DIR}/rootfs.cpio.lzma --cmdline "console=/dev/tty0" --base 0x40000000 --pagesize 2048 -o ${BINARIES_DIR}/boot.img
+#${BR2_EXTERNAL_MIDAS_PATH}/util/mkbootimg --kernel ${BINARIES_DIR}/zImage.exynos4412-i9300 --ramdisk ${BINARIES_DIR}/rootfs.cpio.lzma --cmdline "console=/dev/tty0" --base 0x40000000 --pagesize 2048 -o ${BINARIES_DIR}/boot.img
 
-${BR2_EXTERNAL_MIDAS_PATH}/board/midas/create_4412_sd_image.sh ${BR2_EXTERNAL_MIDAS_PATH}/board/midas/p4412_s_fwbl1.bin ${BINARIES_DIR}/i9300-spl.bin ${BINARIES_DIR}/u-boot-dtb.bin ${BINARIES_DIR}/sd_image.bin
+BOARD_DIR=$(dirname $0)
+
+mkimage=$HOST_DIR/bin/mkimage
+
+run() { echo "$@"; "$@"; }
+die() { echo "$@" >&2; exit 1; }
+test -f $BINARIES_DIR/zImage || die "no zImage"
+test -x $mkimage || die "no mkimage (need host-uboot-tools)"
+
+run cp $BOARD_DIR/kernel.its $BINARIES_DIR/kernel.its || exit 1
+echo "Entering $BINARIES_DIR for next command..."
+(cd $BINARIES_DIR && run $mkimage -f kernel.its boot.itb) || exit 1
+rm -f $BINARIES_DIR/kernel.its
+
+#sh ${BR2_EXTERNAL_MIDAS_PATH}/board/midas/create_4412_sd_image.sh \
+#    ${BR2_EXTERNAL_MIDAS_PATH}/board/midas/p4412_s_fwbl1.bin \
+#    ${BINARIES_DIR}/i9300-spl.bin ${BINARIES_DIR}/u-boot-dtb.bin \
+#    ${BINARIES_DIR}/sdcard_bootloader.bin
+
+sh ${BR2_EXTERNAL_MIDAS_PATH}/board/midas/create_4412_emmc_image.sh \
+    ${BR2_EXTERNAL_MIDAS_PATH}/board/midas/p4412_s_fwbl1.bin \
+    ${BINARIES_DIR}/i9300-spl.bin ${BINARIES_DIR}/u-boot.bin \
+    ${BINARIES_DIR}/bootloader.bin
+
